68f70523bdbebb2402a75ecb47acf45381e5022d
beautus <gumedesduduzo@gmail.com>
2021-05-20 21:25:56 +0200

    removed husky

---
+++ dist/esm/dto/index.js
@@ -0,0 +1,3 @@
+export * from './users/fetch-user-details-response.dto';
+export * from './preferences/base-preferences';
+export * from './preferences/get-organization-preferences-response.dto';
--- src/dto/preferences/base-preferences.ts
+++ src/dto/preferences/base-preferences.ts
@@ -2,4 +2,8 @@ export class BasePreferences {
   toEntries() {
     return Object.entries(this);
   }
+
+  toMap() {
+    return new Map(this.toEntries());
+  }
 }
