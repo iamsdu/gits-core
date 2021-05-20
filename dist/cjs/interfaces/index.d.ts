3954c166fb7ab64e5e275066cb47182678043da6
beautus <gumedesduduzo@gmail.com>
2021-05-20 22:37:37 +0200

    Cheese

---
+++ dist/cjs/interfaces/index.d.ts
@@ -0,0 +1,14 @@
+import { RoleTypes } from '../enums';
+export interface IResponse {
+    success: boolean;
+}
+export interface IGenericResponse {
+    success?: boolean;
+}
+export interface IApplicationRoles {
+    type: RoleTypes;
+}
+export interface IOrganizationPreferences {
+    createOptionVisible: boolean;
+    requireApprovalOnCreate: boolean;
+}
