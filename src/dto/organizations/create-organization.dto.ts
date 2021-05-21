98db016fbcabceab69e9327b23a544ddd666dc06
beautus <gumedesduduzo@gmail.com>
2021-05-21 10:00:54 +0200

    added validation class

--- src/dto/organizations/create-organization.dto.ts
+++ src/dto/organizations/create-organization.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreateOrganizationDto {
+  @IsNotEmpty()
+  @IsString()
   name!: string;
+
+  @IsEmail()
   email!: string;
 }
